0812c94d3f28628fa8964cdfa5752c1fcc12c658
yousef_ayman <79840578+ahmedsayed121@users.noreply.github.com>
2024-09-04 17:40:33 +0300

    Add project files.

---
+++ AppStructuer/Controllers/MovieController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppStructuer.Controllers
+{
+	public class MovieController : Controller
+	{
+		public IActionResult GetMovie(int id)
+		{
+			//ContentResult Result = new ContentResult();
+			//Result.Content = $"Movie With id ={id}";
+			//return Result;
+			//return $"Movie With id ={id}";
+			return Content($"Movie With id ={id}","txt/html");
+
+		}
+		public IActionResult Index()
+		{
+			//RedirectResult ReResult = new RedirectResult("https://localhost:44343/Movie/GetMovie/10");
+			//return ReResult;
+			//return "Index";
+			//return Redirect("{BaseUrl}/Movie/GetMovie/10");
+			return RedirectToAction(nameof(GetMovie), new {id=10});
+		}
+		public ViewResult Hamada()
+		{
+			return View();
+		}
+	}
+}
---
+++ AppStructuer/Startup.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing.Constraints;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace AppStructuer
+{
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllers();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseRouting();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapGet("/", async context =>
+				{
+					await context.Response.WriteAsync("Hello World!");
+				});
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{Controller=Movie}/{action=Index}/{id:int?}"
+					//constraints: new { id = new IntRouteConstraint()}
+					); 
+			});
+
+		}
+	}
+}
